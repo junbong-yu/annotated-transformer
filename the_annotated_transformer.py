b12819ae5e0f599c4a85c4b1b7fcef5d93ecd503
Junbong Yu <junbong.yu@samsung.com>
2025-12-31 20:44:28 +0900

    fix: add guide for installing latest transformers

--- the_annotated_transformer.py
+++ the_annotated_transformer.py
@@ -97,6 +97,9 @@
 
 # %% id="NwClcbH6Tsp8"
 # # !pip install -r requirements.txt
+# # !git clone https://github.com/huggingface/transformers.git
+# # !cd transformers
+# # !pip install -e .
 # ! wget https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.2.0/en_core_web_sm-3.2.0-py3-none-any.whl
 # ! pip install en_core_web_sm-3.2.0-py3-none-any.whl
 # ! wget https://github.com/explosion/spacy-models/releases/download/de_core_news_sm-3.2.0/de_core_news_sm-3.2.0-py3-none-any.whl
